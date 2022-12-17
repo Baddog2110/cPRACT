de2e7c2726fda01949588e347afb39ef4ab4d7b9
Baddog2110 <74144042+Baddog2110@users.noreply.github.com>
2022-12-17 17:42:37 +0300

    Add files via upload

--- blackbox.c
+++ blackbox.c
@@ -6,7 +6,7 @@
 
 int main()
 {
-    int b[9][9];
+    int b[8][8];
     int n = 0, r = 0, x = 0, y = 0, x1 = 0, x2 = 0, x3 = 0, ii = 0, jj = 0;
     int s = 0, c = 0, y1 = 0, y2 = 0, y3 = 0, u = 0, v = 0, z = 0;
     char a[10];
@@ -27,8 +27,8 @@ n150:
     for (int i = 1; i < n + 1;i++)
     {
         n180:
-        x = rand() % 9;
-        y = rand() % 9;
+        x = rand() % 8;
+        y = rand() % 8;
         if (b[x][y] != 0) goto n180;
         b[x][y] = 1;
     }
@@ -37,19 +37,19 @@ n210:
     scanf("%d", &r);  
     while (r > 0)
     {
-        if((r-1)/8+1 > 0 & (r-1)/8+1 <= 1)
+        if((r-1)/8 + 1 == 1)
         {
             goto n240;
         }
-        else if((r-1)/8+1 > 1 & (r-1)/8+1 <= 2)
+        else if((r-1)/8 + 1 == 2)
         {
             goto n250;
         }
-        else if((r-1)/8+1 > 2 & (r-1)/8+1 <= 3)
+        else if((r-1)/8 + 1 == 3)
         {
             goto n260;
         }
-        else if((r-1)/8+1 > 3 & (r-1)/8+1 <= 4)
+        else if((r-1)/8 + 1 == 4)
         {
             goto n270;
         }
@@ -80,19 +80,19 @@ n210:
         }
         y2 = y1 - 1, y3 = y1 + 1, x2 = x1, x3 = x1;
     n310:
-        if(8*b[x1][y1]+b[x2][y2]+2*b[x3][y3] > 0 & 8*b[x1][y1]+b[x2][y2]+2*b[x3][y3] <= 1)
+        if(8*b[x1][y1]+b[x2][y2]+2*b[x3][y3] + 1 == 1)
         {
             goto n330;
         }
-        else if(8*b[x1][y1]+b[x2][y2]+2*b[x3][y3] > 1 & 8*b[x1][y1]+b[x2][y2]+2*b[x3][y3] <= 2)
+        else if(8*b[x1][y1]+b[x2][y2]+2*b[x3][y3] + 1 == 2)
         {
             goto n340;
         }
-        else if(8*b[x1][y1]+b[x2][y2]+2*b[x3][y3] > 2 & 8*b[x1][y1]+b[x2][y2]+2*b[x3][y3] <= 3)
+        else if(8*b[x1][y1]+b[x2][y2]+2*b[x3][y3] + 1 == 3)
         {
             goto n350;
         }
-        else if(8*b[x1][y1]+b[x2][y2]+2*b[x3][y3] > 3 & 8*b[x1][y1]+b[x2][y2]+2*b[x3][y3] <= 4)
+        else if(8*b[x1][y1]+b[x2][y2]+2*b[x3][y3] + 1 == 4)
         {
             goto n340;
         }
@@ -113,33 +113,33 @@ n210:
     n360:
         if (u==0)
         {
-        u=z, v=0; 
+            u=z, v=0; 
             goto n380;
         }
-        u - 0, v = z;
+        u = 0, v = z;
     n380:
-        if((x+15)/8 > 0 & (x+15)/8 <= 1)
+        if((x+15)/8 == 1)
         {
             goto n420;
         }
-        else if((x+15)/8 > 1 & (x+15)/8 <= 2)
+        else if((x+15)/8 == 2)
         {
             goto n400;
         }
-        else if((x+15)/8 > 2 & (x+15)/8 <= 3)
+        else if((x+15)/8 == 3)
         {
             goto n430;
         }
     n400:
-        if((y+15)/8 > 0 & (y+15)/8 <= 1)
+        if((y+15)/8 == 1)
         {
             goto n440;
         }
-        else if((y+15)/8 > 1 & (y+15)/8 <= 2)
+        else if((y+15)/8 == 2)
         {
             goto n280;
         }
-        else if((y+15)/8 > 2 & (y+15)/8 <= 3)
+        else if((y+15)/8 == 3)
         {
             goto n450;
         }
@@ -163,6 +163,7 @@ n210:
         }
         printf("TO ");
         printf("%d",z);
+        printf("\n");
         s = s + 2;
         goto n210;
     }
@@ -207,7 +208,7 @@ n480:
     printf(" OUT OF ");
     printf("%d",n);
     printf(" ATOMS CORRECTLY!! \n");
-    printf(" YOUR SCORE FOR THIS ROUND WAS");
+    printf(" YOUR SCORE FOR THIS ROUND WAS ");
     printf("%d",s);
     printf(" POINTS.");
     printf("\n CARE TO TRY AGAIN (y/no) ");
